d8b2edb1a7e78958803b6967e92e30623eef43ce
BlueNightFury46 <Celestial@shadowtrilogy.co.uk>
2024-03-12 12:09:02 +0000

    Added Password.cs

---
+++ Password.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace shadowtrilogy.co.uk;
+
+class Password {
+
+      public static bool isTyped(String password, bool CaseSensitive){
+
+        if(password.Equals("")){
+          Console.WriteLine("Error! Password cannot be empty; contact the developer to change this");
+          return false;
+        }
+             
+           string? typed = Console.ReadLine();
+
+          //Checks if CaseSensitive mode is on
+           if(CaseSensitive){
+
+            //Checks if the inputed string is equal to the typed string              
+               if(password.Equals(typed)){
+                //if so returns true;
+                return true;
+
+               } else {
+
+                return false;
+               }
+
+         //If CaseSensitive mode is off
+           } else {
+    
+
+          //Attempts to allow s to be null
+          string? s = typed;
+          
+          //Then does a try-catch
+          try{
+            //Then makes sure the password isn't empty
+            if(!s.Equals("")){
+              if(s.ToLower().Equals(password.ToLower())){
+
+                return true;
+              } 
+                else{
+
+                return false;
+              }
+            } else if(s.Equals("")){
+            Console.WriteLine("Please input a value... ");
+            return false;
+                
+            }
+           } catch(NullReferenceException){
+            Console.WriteLine("Please input a value... ");
+            return false;
+           }
+           }
+           return false;
+      }
+
+
+
+
+      public static bool isTyped(String password){
+             
+           string? typed = Console.ReadLine();
+
+          //Checks if CaseSensitive mode is on
+          
+
+            //Checks if the inputed string is equal to the typed string              
+               if(password.Equals(typed)){
+                //if so returns true;
+                return true;
+
+               } else {
+
+                return false;
+               }
+      }
+}
